8cb501745b1131fa7f7e4466b1c086a1f473e79b
KIMMUSIC <ldgyuuuuu@gmail.com>
2021-01-18 15:34:58 +0900

    delete

--- Hello.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-int main(void)
-{
-    cout << "JHHH" << endl;
-}
--- T2.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main(void)
-{
-    int t = 1;
-    cout << t << endl;
-}
